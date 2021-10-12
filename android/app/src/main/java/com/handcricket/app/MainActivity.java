044d26f027a67aad3ec69776506101f88a7ab6f3
k4u5h4l <kaushal.v.bhat@gmail.com>
2021-10-12 13:16:20 +0530

    add capacitor

---
+++ android/app/src/main/java/com/handcricket/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.handcricket.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.handcricket.app',
+  appName: 'Hand Cricket',
+  webDir: 'out',
+  bundledWebRuntime: false
+};
+
+export default config;
